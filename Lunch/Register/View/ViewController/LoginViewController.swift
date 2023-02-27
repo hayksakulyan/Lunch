a4894342ea6191163fe6e7922826466650d12d08
hayksakulyan <sakulyanhayk@gmail.com>
2023-02-27 21:08:34 +0400

    "This is first commit of Lunch application"

---
+++ Lunch/Register/View/ViewController/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Lunch
+//
+//  Created by Hayk Sakulyan on 27.02.23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
